14bdaa3cd729a2a9c801dd3d2c2c2a5171aa2fc2
yyx9608 <757825921@qq.com>
2022-09-16 01:18:57 +0800

    代理配置

--- src/plugin/axios/index.ts
+++ src/plugin/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://1486641sd0.iask.in:35485';
 
+const str = process.env.NODE_ENV === 'development' ? '/devServer' : 'http://1486641sd0.iask.in:35485'
+axios.defaults.baseURL = str;
 
 
 export default axios;
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+
+    proxy: {
+      '/devServer': {
+        target: 'http://1486641sd0.iask.in:35485',
+        ws: true,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/devServer/, '')
+      },
+    }
+
+  }
 })
