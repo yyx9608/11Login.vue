e6b2c46b06ad7c17e2e897ae7fe334ed6439b329
yyx9608 <757825921@qq.com>
2022-10-05 14:50:33 +0800

    完成登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,7 +13,7 @@
         </el-form-item>
       </el-form>
       <div>
-        <el-button type="primary" @click="loginBoke">登陆</el-button>
+        <el-button type="primery" @click="loginBoke">登陆</el-button>
       </div>
     </div>
   </div>
@@ -22,25 +22,25 @@
 <script lang="ts" setup>
 import { ref } from '@vue/reactivity';
 import { login } from '../plugin/axios/interface';
-import { ElMessage } from 'element-plus';
+
+
 import router from '../router';
 
 
 // 用户名
-const username = ref('');
-const password = ref('');
+const username = ref('test1');
+const password = ref('123456');
 async function loginBoke() {
   const { data: res } = await login({
     username: username.value,
     password: password.value,
   });
-  if (res.meta.status === 1) {
-    return ElMessage.error(res.meta.msg);
+
+  // 1000为登录成功
+  if (res.code === 1000) {
+    router.push({ name: 'main' });
   }
-  const data = res.data;
-  data.uname = username.value;
-  sessionStorage.setItem('loginInfo', JSON.stringify(data));
-  router.push({ name: 'main' });
+
 }
 </script>
 
@@ -51,6 +51,7 @@ async function loginBoke() {
   background: url(../../public/background.png) no-repeat;
   background-size: 100%;
 }
+
 .login {
   width: 30%;
   height: 500px;
@@ -61,22 +62,27 @@ async function loginBoke() {
   transform: translate(-50%, -50%);
   color: rgb(63, 61, 61);
 }
-.btn { /*登录按钮*/
+
+.btn {
+  /*登录按钮*/
   display: inline-block;
   text-align: center;
 
   transform: translate(-50%, -50%);
   /*display:flex;*/
   padding: 5px;
-  width: 80%; /*长度*/
+  width: 80%;
+  /*长度*/
   height: 150%;
   margin: auto;
-  margin-top: 1px; /*边框与上面的距离*/
+  margin-top: 1px;
+  /*边框与上面的距离*/
   margin-left: 190px;
   margin-right: 210px;
   background-color: darkred;
   color: #492626;
 }
+
 :deep(.el-form) {
   margin-top: 30px;
   margin-bottom: 30px;
